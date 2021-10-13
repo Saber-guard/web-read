6c1c11c19c81bacb4b3d08549b2b727bbffebe10
kaellzhao <kaellzhao@tencent.com>
2021-10-13 18:42:01 +0800

    fix:解决跨域问题

--- middleware/Cors.go
+++ middleware/Cors.go
@@ -7,7 +7,10 @@ import (
 
 func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		origin := c.Request.Header["Origin"][0]
+		origin := "*"
+		if len(c.Request.Header["Origin"]) > 0 {
+			origin = c.Request.Header["Origin"][0]
+		}
 		c.Header("Access-Control-Allow-Origin", origin)
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Cookie, Accept")
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, OPTIONS")
--- service/SharesService.go
+++ service/SharesService.go
@@ -8,8 +8,16 @@ import (
 type SharesService struct {
 }
 
-func (c SharesService) List() []model.CompanyInfoModel {
+func (c SharesService) List() []map[string]interface{} {
 	var companyList []model.CompanyInfoModel
 	DbService.Db.Scopes(util.PaginatorUtil{Page: 1, Size: 100}.Exec).Find(&companyList)
-	return companyList
+	list := make([]map[string]interface{}, len(companyList))
+	for index, item := range companyList {
+		list[index] = map[string]interface{}{
+			"id":   item.ID,
+			"code": item.Code,
+			"name": item.Name,
+		}
+	}
+	return list
 }
